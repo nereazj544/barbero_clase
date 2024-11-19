f9988688778bf69e8bafedcb2ff32c786476e3d9
nereazj544 <nzjvirtual@gmail.com>
2024-11-19 13:26:05 +0100

    n

--- src/fp/dam/psp/barbero/Barberia.java
+++ src/fp/dam/psp/barbero/Barberia.java
@@ -1,13 +1,17 @@
 package fp.dam.psp.barbero;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Barberia {
     Semaphore salaEspera;
     Sillon sillon;
 
+    ReentrantLock lock = new ReentrantLock();
+
     public Barberia(int sillas) {
         salaEspera = new Semaphore(sillas);
+        sillon = new Sillon();
     }
 
     public boolean entrarEnSalaDeEspera() {
@@ -18,7 +22,27 @@ public class Barberia {
     }
 
     public void solicitarCortePelo() {
-
+        lock.lock();
+        try {
+            sillon.ocp();
+            salaEspera.release();
+        } finally {
+            lock.unlock();
+            
+        }
     }
 
+    public void finalCP() {
+        sillon.des();
+    }
+
+    public void esCnt() throws InterruptedException{
+        sillon.esCli();
+    }
+
+    public boolean sillonOc(){
+        return sillon.estaOc();
+    }
+
+    // ! END CLASS
 }
--- src/fp/dam/psp/barbero/Sillon.java
+++ src/fp/dam/psp/barbero/Sillon.java
@@ -1,4 +1,59 @@
 package fp.dam.psp.barbero;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Sillon {
+
+    boolean oc = false;
+    ReentrantLock lock = new ReentrantLock();
+    Condition c = lock.newCondition();
+
+    public void ocp() {
+        // TODO Auto-generated method stub
+        lock.lock();
+        try {
+            oc = true;
+            c.signal();
+        } finally {
+            // TODO: handle exception
+            lock.unlock();
+        }
+    }
+
+    public void des() {
+        // TODO Auto-generated method stub
+        lock.lock();
+        try {
+            oc = false;
+
+        } finally {
+            // TODO: handle exception
+            lock.unlock();
+        }
+
+    }
+
+    public void esCli() throws InterruptedException {
+        lock.lock();
+        try {
+            while (!oc) {
+                c.await();
+            }
+        } finally {
+            // TODO: handle exception
+        }
+    }
+
+    public boolean estaOc() {
+        lock.lock();
+        try {
+            return oc;
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    // ! EDN CLASS
 }
